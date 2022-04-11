c984ed9fbcdf79cb94bb0840a781c6c699cde539
SajuHemanth <96937846+SajuHemanth@users.noreply.github.com>
2022-04-11 17:44:52 +0530

    Add files via upload

---
+++ 11-04-2022/+quantifiers.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+
+$a = "Sds";
+	
+if ($a =~ m/Se+ds/)
+{
+	print "Match Found\n";
+}
+else
+{
+	print "Match Not Found\n";
+}
---
+++ 11-04-2022/?quantifiers.pl
@@ -0,0 +1,12 @@
+#!/usr/bin/perl
+
+$a = "Seeds";
+
+if ($a =~ m/Se?ds/)
+{
+	print "Match Found\n";
+}
+else
+{
+	print "Match Not Found\n";
+}
