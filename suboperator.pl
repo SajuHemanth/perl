8d14ac2b78bef342c5dc84684253523cb1e5f153
SajuHemanth <96937846+SajuHemanth@users.noreply.github.com>
2022-04-11 17:45:37 +0530

    Delete suboperator.pl

--- suboperator.pl
+++
@@ -1,8 +0,0 @@
-#/user/bin/perl
-
-$string1 = 'welcome to aspire family';
-$string2 = 'Hello All !!! welcome here';
-$string1 =~ s/family/system/;
-$string2 =~ s/here/to all/;
-print "$string1\n";
-print "$string2\n";
